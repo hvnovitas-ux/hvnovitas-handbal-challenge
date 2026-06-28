d3f7b116770e8be0a4ce02d7a9902dc2a0cbfba6
hvnovitas-ux <hvnovitas@gmail.com>
2026-06-28 22:08:25 +0200

    Duplicate questions in questions.js

--- questions.js
+++ questions.js
@@ -1,54 +1,54 @@
 ```javascript
 const questions = [
-  {
-    q: "Hoeveel spelers staan er per team in het veld?",
-    a: ["5", "6", "7", "8"],
-    c: 2
-  },
-  {
-    q: "Welke kleur draagt HV Novitas?",
-    a: ["Blauw", "Groen", "Oranje", "Rood"],
-    c: 2
-  },
-  {
-    q: "Waar speelt HV Novitas?",
-    a: ["Hulst", "Goes", "Middelburg", "Terneuzen"],
-    c: 0
-  },
-  {
-    q: "Wie verdedigt het doel?",
-    a: ["Cirkel", "Keeper", "Hoek", "Coach"],
-    c: 1
-  },
-  {
-    q: "Hoeveel helften heeft een handbalwedstrijd?",
-    a: ["1", "2", "3", "4"],
-    c: 1
-  },
-  {
-    q: "Vanaf welke lijn wordt een strafworp genomen?",
-    a: ["6 meter", "7 meter", "8 meter", "9 meter"],
-    c: 1
-  },
-  {
-    q: "Hoeveel stappen mag je zetten zonder te stuiten?",
-    a: ["2", "3", "4", "5"],
-    c: 1
-  },
-  {
-    q: "Welke sport speelt HV Novitas?",
-    a: ["Volleybal", "Handbal", "Basketbal", "Korfbal"],
-    c: 1
-  },
-  {
-    q: "Hoeveel punten krijg je voor een overwinning?",
-    a: ["1", "2", "3", "0"],
-    c: 1
-  },
-  {
-    q: "Wat is de slogan van HV Novitas?",
-    a: ["No Stress, Enjoy!", "Play Hard", "Orange Power", "Handball Forever"],
-    c: 0
-  }
+    {
+        q: "Hoeveel spelers staan er per team in het veld?",
+        a: ["5", "6", "7", "8"],
+        c: 2
+    },
+    {
+        q: "Welke kleur draagt HV Novitas?",
+        a: ["Blauw", "Groen", "Oranje", "Rood"],
+        c: 2
+    },
+    {
+        q: "Waar speelt HV Novitas?",
+        a: ["Hulst", "Goes", "Middelburg", "Terneuzen"],
+        c: 0
+    },
+    {
+        q: "Wie verdedigt het doel?",
+        a: ["Cirkel", "Keeper", "Hoek", "Coach"],
+        c: 1
+    },
+    {
+        q: "Hoeveel helften heeft een handbalwedstrijd?",
+        a: ["1", "2", "3", "4"],
+        c: 1
+    },
+    {
+        q: "Vanaf welke lijn wordt een strafworp genomen?",
+        a: ["6 meter", "7 meter", "8 meter", "9 meter"],
+        c: 1
+    },
+    {
+        q: "Hoeveel stappen mag je zetten zonder te stuiten?",
+        a: ["2", "3", "4", "5"],
+        c: 1
+    },
+    {
+        q: "Welke sport speelt HV Novitas?",
+        a: ["Volleybal", "Handbal", "Basketbal", "Korfbal"],
+        c: 1
+    },
+    {
+        q: "Hoeveel punten krijg je voor een overwinning?",
+        a: ["1", "2", "3", "0"],
+        c: 1
+    },
+    {
+        q: "Wat is de slogan van HV Novitas?",
+        a: ["No Stress, Enjoy!", "Play Hard", "Orange Power", "Handball Forever"],
+        c: 0
+    }
 ];
 ```
