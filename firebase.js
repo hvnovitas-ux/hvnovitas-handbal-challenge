8f1fbc08034b39ede2e696e13dff03ec3d10ccc4
hvnovitas-ux <hvnovitas@gmail.com>
2026-06-28 21:18:39 +0200

    Add Firebase configuration and initialization

--- firebase.js
+++ firebase.js
@@ -1,1 +1,22 @@
+```javascript
+// Firebase configuratie
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-database.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBCUZeWMIxIz__7TfNG_b0V47H_pYFPyqQ",
+  authDomain: "hv-novitas-handbal-challenge.firebaseapp.com",
+  databaseURL: "https://hv-novitas-handbal-challenge-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "hv-novitas-handbal-challenge",
+  storageBucket: "hv-novitas-handbal-challenge.firebasestorage.app",
+  messagingSenderId: "707710141199",
+  appId: "1:707710141199:web:ba304ce4e5f653d0afb47a",
+  measurementId: "G-9CHFEC9MSD"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+
+console.log("✅ Firebase verbonden");
+```
 
