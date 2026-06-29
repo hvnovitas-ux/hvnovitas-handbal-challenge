41a5359c1622fc3c732bbe10dd2f96dba615e589
hvnovitas-ux <hvnovitas@gmail.com>
2026-06-29 23:33:42 +0200

    Remove limit on number of scores displayed
    Remove slicing of scores to keep all entries.

--- firebase.js
+++ firebase.js
@@ -75,7 +75,7 @@ scores = Object.values(besteScores);
 
 scores.sort((a, b) => b.score - a.score);
 
-scores = scores.slice(0, 10);
+
 
 const hoogsteScore = scores.length > 0 ? scores[0].score : 0;
 let html = `
