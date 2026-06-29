8bf229a6b9925193230269911a695e126201554a
hvnovitas-ux <hvnovitas@gmail.com>
2026-06-29 15:25:36 +0200

    Update questions.js

--- questions.js
+++ questions.js
@@ -1,53 +1,154 @@
-
 const questions = [
-    {
-        q: "Hoeveel spelers staan er per team in het veld?",
-        a: ["5", "6", "7", "8"],
-        c: 2
-    },
-    {
-        q: "Welke kleur draagt HV Novitas?",
-        a: ["Blauw", "Groen", "Oranje", "Rood"],
-        c: 2
-    },
-    {
-        q: "Waar speelt HV Novitas?",
-        a: ["Hulst", "Goes", "Middelburg", "Terneuzen"],
-        c: 0
-    },
-    {
-        q: "Wie verdedigt het doel?",
-        a: ["Cirkel", "Keeper", "Hoek", "Coach"],
-        c: 1
-    },
-    {
-        q: "Hoeveel helften heeft een handbalwedstrijd?",
-        a: ["1", "2", "3", "4"],
-        c: 1
-    },
-    {
-        q: "Vanaf welke lijn wordt een strafworp genomen?",
-        a: ["6 meter", "7 meter", "8 meter", "9 meter"],
-        c: 1
-    },
-    {
-        q: "Hoeveel stappen mag je zetten zonder te stuiten?",
-        a: ["2", "3", "4", "5"],
-        c: 1
-    },
-    {
-        q: "Welke sport speelt HV Novitas?",
-        a: ["Volleybal", "Handbal", "Basketbal", "Korfbal"],
-        c: 1
-    },
-    {
-        q: "Hoeveel punten krijg je voor een overwinning?",
-        a: ["1", "2", "3", "0"],
-        c: 1
-    },
-    {
-        q: "Wat is de slogan van HV Novitas?",
-        a: ["No Stress, Enjoy!", "Play Hard", "Orange Power", "Handball Forever"],
-        c: 0
-    }
+
+{
+    q:"Hoeveel spelers staan er per team in het veld?",
+    a:["5","6","7","8"],
+    c:2
+},
+{
+    q:"Welke kleur draagt HV Novitas?",
+    a:["Blauw","Groen","Oranje","Rood"],
+    c:2
+},
+{
+    q:"Waar speelt HV Novitas?",
+    a:["Hulst","Goes","Middelburg","Terneuzen"],
+    c:0
+},
+{
+    q:"Wie verdedigt het doel?",
+    a:["Cirkel","Keeper","Hoek","Coach"],
+    c:1
+},
+{
+    q:"Hoeveel helften heeft een handbalwedstrijd?",
+    a:["1","2","3","4"],
+    c:1
+},
+{
+    q:"Vanaf welke lijn wordt een strafworp genomen?",
+    a:["6 meter","7 meter","8 meter","9 meter"],
+    c:1
+},
+{
+    q:"Hoeveel stappen mag je zetten zonder te stuiten?",
+    a:["2","3","4","5"],
+    c:1
+},
+{
+    q:"Welke sport speelt HV Novitas?",
+    a:["Volleybal","Handbal","Basketbal","Korfbal"],
+    c:1
+},
+{
+    q:"Hoeveel punten krijg je voor een overwinning?",
+    a:["1","2","3","0"],
+    c:1
+},
+{
+    q:"Wat is de slogan van HV Novitas?",
+    a:["No Stress, Enjoy!","Play Hard","Orange Power","Handball Forever"],
+    c:0
+},
+{
+    q:"Hoe heet de lijn op 6 meter?",
+    a:["Doellijn","Doelgebiedlijn","Vrijeworplijn","Middenlijn"],
+    c:1
+},
+{
+    q:"Hoe lang duurt een tijdstraf?",
+    a:["1 minuut","2 minuten","3 minuten","5 minuten"],
+    c:1
+},
+{
+    q:"Welke kaart betekent directe uitsluiting?",
+    a:["Geel","Rood","Blauw","Groen"],
+    c:1
+},
+{
+    q:"Vanaf welke lijn wordt een vrije worp meestal genomen?",
+    a:["6 meter","7 meter","9 meter","10 meter"],
+    c:2
+},
+{
+    q:"Hoeveel scheidsrechters leiden meestal een wedstrijd?",
+    a:["1","2","3","4"],
+    c:1
+},
+{
+    q:"Hoe heet een schot waarbij je springt en voor de landing gooit?",
+    a:["Draaischot","Sprongschot","Onderhands schot","Stuitschot"],
+    c:1
+},
+{
+    q:"Wanneer mag je wisselen?",
+    a:["Alleen in de rust","Na een doelpunt","Altijd via de wisselzone","Alleen na fluitsignaal"],
+    c:2
+},
+{
+    q:"Hoeveel spelers staan er tegelijk van één team in het veld inclusief keeper?",
+    a:["6","7","8","9"],
+    c:1
+},
+{
+    q:"Welke speler staat meestal midden in de aanval?",
+    a:["Hoekspeler","Cirkelspeler","Middenopbouwer","Keeper"],
+    c:2
+},
+{
+    q:"Welke maat bal gebruiken senior heren?",
+    a:["1","2","3","4"],
+    c:2
+},
+{
+    q:"Welke maat bal gebruiken senior dames?",
+    a:["1","2","3","4"],
+    c:1
+},
+{
+    q:"Wat gebeurt er als een verdediger de aanvaller in de cirkel duwt?",
+    a:["Doorspelen","Vrije worp","Strafworp","Doelpunt"],
+    c:2
+},
+{
+    q:"Wat betekent passief spel?",
+    a:["Te langzaam aanvallen zonder doelpoging","Alleen verdedigen","Veel wisselen","Bal laten stuiten"],
+    c:0
+},
+{
+    q:"Hoeveel seconden mag je de bal vasthouden zonder te dribbelen of te passen?",
+    a:["2","3","4","5"],
+    c:1
+},
+{
+    q:"Welke positie verdedigt het doel?",
+    a:["Cirkelspeler","Keeper","Middenopbouwer","Hoekspeler"],
+    c:1
+},
+{
+    q:"Wat is de maximale score voor deze quiz?",
+    a:["20","25","30","50"],
+    c:2
+},
+{
+    q:"In welke provincie ligt Hulst?",
+    a:["Noord-Brabant","Zeeland","Limburg","Zuid-Holland"],
+    c:1
+},
+{
+    q:"Waar staat HV Novitas bekend om?",
+    a:["Jeugdopleiding","Voetbal","Hockey","Volleybal"],
+    c:0
+},
+{
+    q:"Welke kleur hoort bij HV Novitas?",
+    a:["Blauw","Rood","Oranje","Paars"],
+    c:2
+},
+{
+    q:"Wat is het belangrijkste motto van HV Novitas?",
+    a:["Samen winnen","No Stress, Enjoy!","Altijd aanvallen","Nooit opgeven"],
+    c:1
+}
+
 ];
