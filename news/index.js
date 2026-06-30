14707b4e88383228f27b1395b7d39237089725da
hvnovitas-ux <hvnovitas@gmail.com>
2026-06-30 17:34:00 +0200

    Add Firebase integration to fetch news articles

---
+++ news/index.js
@@ -0,0 +1,60 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
+import {
+    getDatabase,
+    ref,
+    query,
+    orderByChild,
+    limitToLast,
+    onValue
+} from "https://www.gstatic.com/firebasejs/12.1.0/firebase-database.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBCUZeWMIxIz__7TfNG_b0V47H_pYFPyQ",
+    authDomain: "hv-novitas-handbal-challenge.firebaseapp.com",
+    databaseURL: "https://hv-novitas-handbal-challenge-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "hv-novitas-handbal-challenge",
+    storageBucket: "hv-novitas-handbal-challenge.firebasestorage.app",
+    messagingSenderId: "707710141199",
+    appId: "1:707710141199:web:ba304ce4e5f653d0afb47a"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+
+const nieuws = document.getElementById("news");
+
+const q = query(
+    ref(db, "news"),
+    orderByChild("created"),
+    limitToLast(3)
+);
+
+onValue(q, (snapshot) => {
+
+    let berichten = [];
+
+    snapshot.forEach((item) => {
+        berichten.push(item.val());
+    });
+
+    berichten.reverse();
+
+    let html = "";
+
+    berichten.forEach((b) => {
+
+        const datum = new Date(b.created).toLocaleDateString("nl-NL");
+
+        html += `
+        <div class="bericht">
+            <h2>${b.title}</h2>
+            <small>${datum}</small>
+            <p>${b.text}</p>
+        </div>
+        <hr>
+        `;
+    });
+
+    nieuws.innerHTML = html;
+
+});
